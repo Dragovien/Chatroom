9ec31b227b1c736b75c3dbeb82ca54f07c04c11c
Jo-han <jo-han@miloctav.fr>
2023-02-08 16:14:43 +0100

    Added Back, redo Chatroom.vue + router

---
+++ api/server.js
@@ -0,0 +1,24 @@
+
+const app = require('express')();
+const registeredUsers = [];
+
+app.set("port", process.env.PORT || 3000);
+
+var http = require("http").Server(app);
+var io = require("socket.io")(http, {
+    cors: {
+        origin : "*",
+    }
+});
+
+io.on("connection", function(socket){
+    console.log("connected");
+    socket.on("message", (message) => {
+        console.log(socket.emit("messageReceived", message));
+        console.log(message);
+    })
+})
+
+const server = http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
---
+++ front/src/pages/ChatPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ChatRoom></ChatRoom>
+    </div>
+</template>
+
+<script setup>
+import ChatRoom from '../components/ChatRoom.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
